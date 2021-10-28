3209948beeeffb3e30b4f542d5c738a7ce721b6c
Jordan Roberts <jdrobs19@gmail.com>
2021-10-28 12:11:55 -0600

    began models

---
+++ Models/Tasks.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Controllers.Models
+{
+    public class Tasks
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Priority { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
