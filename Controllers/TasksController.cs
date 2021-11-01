796278bb9c9df57a9cca532e861eb8631e88ac36
Jordan Roberts <jdrobs19@gmail.com>
2021-11-01 13:50:28 -0600

    fixing CRUDs

--- Controllers/TasksController.cs
+++ Controllers/TasksController.cs
@@ -1,88 +1,68 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using wtw_task_challenge.utils;
 
-
-namespace wtw_task_challenge.Controllers {
-
-
-    [ApiController]
+namespace wtw_task_challenge.Controllers
+{
+    // [ApiController]
     [Route("[controller]")]
     public class TasksController : ControllerBase
     {
         private ToDoListManager toDoListManager;
 
+        //get all tasks
         [HttpGet]
-
         public IEnumerable<Tasks> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new Tasks()
-            {
-                Title = "Test",
-                Description = "This is a test",
-                Priority = "Medium",
-                Due = DateTime.Today
-            })
-            .ToArray();
+            toDoListManager = new ToDoListManager();
+            return (IEnumerable<Tasks>)toDoListManager.GetTasks();
         }
-    //Get task with specific id
-    [HttpGet("{id}")]
-    public IActionResult Get(int id)
-    {
-        return Ok(new Tasks()
+
+
+        // Get task with specific id
+        [HttpGet("id")]
+        public IEnumerable<Tasks> GetTask(Tasks id)
         {
-            Title = "Test",
-            Description = "This is a test",
-            Status = "Not Started",
-            Priority = "Medium",
-            Due = DateTime.Today
-        });
-    }
+            toDoListManager = new ToDoListManager();
+            return (IEnumerable<Tasks>)toDoListManager.GetTask(id);
+        }
 
-    //Post
-    [HttpPost]
-    public IActionResult Post([FromBody] Tasks task)
-    {
-        //Add task to database
-        toDoListManager.AddTask(task);
-        return Ok(
-            task
-        );
-    }   
-
-    //Put task with specific id
-    [HttpPut("{id}")]
-    public IActionResult Put(int id, [FromBody] Tasks task)
-    {
-        return Ok( 
-        task
-        );
-    }
-
-    //Delete task with specific id
-    [HttpDelete("{id}")]
-    public IActionResult Delete(int id)
-    {
-        return Ok(new Tasks()
+        //Get task with specific status
+        [HttpGet("status/{status}")]
+        public IEnumerable<Tasks> GetTasksByStatus(Tasks status)
         {
-            Title = "Test",
-            Description = "This is a test",
-            Status = "Started",
-            Priority = "Medium",
-            Due = DateTime.Today
-        });
+            toDoListManager = new ToDoListManager();
+            return (IEnumerable<Tasks>)toDoListManager.GetTasksByStatus(status);
+        }
+
+        //Post
+        [HttpPost]
+        public void AddTask([FromBody] Tasks task)
+        {
+            //Add task to database
+            toDoListManager = new ToDoListManager();
+            toDoListManager.AddTask(task);
+        }
+
+        // Put task with specific id
+        [HttpPut("id")]
+        public void Put(Tasks id, [FromBody] Tasks task)
+        {
+            //Update task in database
+            toDoListManager = new ToDoListManager();
+            toDoListManager.UpdateTask(id, task);
+
+        }
+
+        //Delete task with specific id
+        [HttpDelete("id")]
+        public void Delete(Tasks id)
+        {
+            //Delete task from database
+            toDoListManager = new ToDoListManager();
+            toDoListManager.DeleteTask(id);
+        }
     }
-}
 }
--- utils/databasecontroller.cs
+++ utils/databasecontroller.cs
@@ -1,121 +1,223 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
-namespace wtw_task_challenge.utils{
+namespace wtw_task_challenge.utils
+{
+    public class DatabaseController
+    {
+        private static string
+            connectionString =
+                "Server=localhost;Database=wtw_task_challenge;Trusted_Connection=True";
 
-    public class DatabaseController{
-        private static string connectionString = "Server=localhost;Database=wtw_task_challenge;Trusted_Connection=True";
-        private MySqlConnection connection;
-        private MySqlCommand command;
-        private MySqlDataReader reader;
+        private SqlConnection connection;
 
-        public DatabaseController(){
-            connection = new MySqlConnection(connectionString);
+        private SqlCommand command;
+
+        private SqlDataReader reader;
+
+        public DatabaseController()
+        {
+            connection = new SqlConnection(connectionString);
         }
 
-        public void OpenConnection(){
+        public void OpenConnection()
+        {
             connection.Open();
         }
 
-        public void CloseConnection(){
-            connection.Close();
-        }
-
-        public void ExecuteQuery(string query){
-            command = new MySqlCommand(query, connection);
+        public void ExecuteQuery(string query)
+        {
+            command = new SqlCommand(query, connection);
             command.ExecuteNonQuery();
         }
 
-        public MySqlDataReader ExecuteQueryWithReturn(string query){
-            command = new MySqlCommand(query, connection);
+        public SqlDataReader ExecuteQueryWithReturn(string query)
+        {
+            command = new SqlCommand(query, connection);
             reader = command.ExecuteReader();
             return reader;
         }
+
+        public void CloseConnection()
+        {
+            connection.Close();
+        }
     }
 
-    public class ToDoListManager{
+    public class ToDoListManager
+    {
         private DatabaseController controller;
 
-        public void AddTask(Tasks task){
+        public ToDoListManager()
+        {
             controller = new DatabaseController();
-            controller.OpenConnection();
-            controller.ExecuteQuery("INSERT INTO [dbo].[tasks] ([title], [description], [status], [priority], [due_date]) VALUES ('" + task.Title + "','" + task.Description + "','" + task.Status + "','" + task.Priority + "','" + task.Due + "')");
-            controller.CloseConnection();
         }
 
-        public void DeleteTask(Tasks task){
+        public void AddTask(Tasks task)
+        {
             controller = new DatabaseController();
             controller.OpenConnection();
-            controller.ExecuteQuery("DELETE FROM [dbo].[tasks] WHERE [id] = '" + task.Id + "'");
+            controller
+                .ExecuteQuery("INSERT INTO [dbo].[tasks] ([title], [description], [status], [priority], [due_date]) VALUES ('" +
+                task.Title +
+                "','" +
+                task.Description +
+                "','" +
+                task.Status +
+                "','" +
+                task.Priority +
+                "','" +
+                task.Due +
+                "')");
             controller.CloseConnection();
+
         }
 
-        public void UpdateTask(Tasks task){
+        public void DeleteTask(Tasks task)
+        {
             controller = new DatabaseController();
             controller.OpenConnection();
-            controller.ExecuteQuery("UPDATE [dbo].[tasks] SET [title] = '" + task.Title + "', [description] = '" + task.Description + "', [status] = '" + task.Status + "', [priority] = '" + task.Priority + "', [due_date] = '" + task.Due + "' WHERE [id] = '" + task.Id + "'");
+            controller
+                .ExecuteQuery("DELETE FROM [dbo].[tasks] WHERE [id] = '" +
+                task.Id +
+                "'");
             controller.CloseConnection();
-    }
 
-        public List<Tasks> GetTasks(){
+        }
+
+        public void UpdateTask(Tasks id, Tasks task)
+        {
+            controller = new DatabaseController();
+            controller.OpenConnection();
+            controller
+                .ExecuteQuery("UPDATE [dbo].[tasks] SET [title] = '" +
+                task.Title +
+                "', [description] = '" +
+                task.Description +
+                "', [status] = '" +
+                task.Status +
+                "', [priority] = '" +
+                task.Priority +
+                "', [due_date] = '" +
+                task.Due +
+                "' WHERE [id] = '" +
+                task.Id +
+                "'");
+            controller.CloseConnection();
+
+        }
+
+        public List<Tasks> GetTasks()
+        {
             List<Tasks> tasks = new List<Tasks>();
             controller = new DatabaseController();
             controller.OpenConnection();
-            MySqlDataReader reader = controller.ExecuteQueryWithReturn("SELECT * FROM [dbo].[tasks]");
-            while(reader.Read()){
+            SqlDataReader reader =
+                controller
+                    .ExecuteQueryWithReturn("SELECT * FROM [dbo].[tasks]");
+            while (reader.Read())
+            {
                 Tasks task = new Tasks();
                 task.Id = reader.GetInt32(0);
                 task.Title = reader.GetString(1);
                 task.Description = reader.GetString(2);
                 task.Status = reader.GetString(3);
                 task.Priority = reader.GetString(4);
-                task.Due = reader.GetString(5);
-                tasks.Add(task);
+                task.Due = reader.GetDateTime(5);
+                tasks.Add (task);
             }
             controller.CloseConnection();
             return tasks;
-}
+        }
 
-        public List<Tasks> GetTasksByStatus(string status){
+        public Tasks GetTask(Tasks id)
+        {
+            Tasks task = new Tasks();
+            controller = new DatabaseController();
+            controller.OpenConnection();
+            SqlDataReader reader =
+                controller
+                    .ExecuteQueryWithReturn("SELECT * FROM [dbo].[tasks] WHERE [id] = '" +
+                    id +
+                    "'");
+            while (reader.Read())
+            {
+                task.Id = reader.GetInt16(0);
+                task.Title = reader.GetString(1);
+                task.Description = reader.GetString(2);
+                task.Status = reader.GetString(3);
+                task.Priority = reader.GetString(4);
+                task.Due = reader.GetDateTime(5);
+            }
+            controller.CloseConnection();
+            return task;
+        }
+
+        public List<Tasks> GetTasksByStatus(Tasks status)
+        {
             List<Tasks> tasks = new List<Tasks>();
             controller = new DatabaseController();
             controller.OpenConnection();
-            MySqlDataReader reader = controller.ExecuteQueryWithReturn("SELECT * FROM [dbo].[tasks] WHERE [status] = '" + status + "'");
-            while(reader.Read()){
+            SqlDataReader reader =
+                controller
+                    .ExecuteQueryWithReturn("SELECT * FROM [dbo].[tasks] WHERE [status] = '" +
+                    status +
+                    "'");
+            while (reader.Read())
+            {
                 Tasks task = new Tasks();
                 task.Id = reader.GetInt32(0);
                 task.Title = reader.GetString(1);
                 task.Description = reader.GetString(2);
                 task.Status = reader.GetString(3);
                 task.Priority = reader.GetString(4);
-                task.Due = reader.GetString(5);
-                tasks.Add(task);
+                task.Due = reader.GetDateTime(5);
+                tasks.Add (task);
             }
             controller.CloseConnection();
             return tasks;
         }
 
-        public void UpdateTaskStatus(string status, string id){
+        public Tasks UpdateTaskStatus(Tasks status, Tasks id)
+        {
+            Tasks task = new Tasks();
             controller = new DatabaseController();
             controller.OpenConnection();
-            controller.ExecuteQuery("UPDATE [dbo].[tasks] SET [status] = '" + status + "' WHERE [id] = '" + id + "'");
+            SqlDataReader reader =
+                controller
+                    .ExecuteQueryWithReturn("UPDATE [dbo].[tasks] SET [status] = '" +
+                    status +
+                    "' WHERE [id] = '" +
+                    id +
+                    "'");
+            while (reader.Read())
+            {
+                task.Id = reader.GetInt32(0);
+                task.Title = reader.GetString(1);
+                task.Description = reader.GetString(2);
+                task.Status = reader.GetString(3);
+                task.Priority = reader.GetString(4);
+                task.Due = reader.GetDateTime(5);
+            }
             controller.CloseConnection();
+            return task;
+
+            // controller = new DatabaseController();
+            // controller.OpenConnection();
+            // controller
+            //     .ExecuteQuery("UPDATE [dbo].[tasks] SET [status] = '" +
+            //     status +
+            //     "' WHERE [id] = '" +
+            //     id +
+            //     "'");
+            // controller.CloseConnection();
+            // return tasks;
+        }
     }
 }
-}
-    
+
 // public class DataConn(Tasks tasks, string query)
 //         {
 //             string connetionString;
@@ -132,7 +234,7 @@ namespace wtw_task_challenge.utils{
 //                 }
 //             catch (Exception e)
 //                 {
-              
+
 //                 string help = "";
 //                     //Error when save data
 //                     //MessageBox.Show("Error to save on database");  //Error when save data
