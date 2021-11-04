b3e0438c9dd1ed7442ddcc81a86797b9a0526eeb
Jordan Roberts <jdrobs19@gmail.com>
2021-11-03 20:42:47 -0600

    tests push

--- __tests__/TasksControllerTests.cs
+++ __tests__/TasksControllerTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data.SqlClient;
 using Xunit;
+using wtw_task_challenge.utils;
 
 namespace wtw_task_challenge
 {
@@ -85,23 +86,105 @@ namespace wtw_task_challenge
         }
 
         //Delete a row in the table
+        // [Fact]
+        // public void DeleteRow()
+        // {
+        //     // Arrange
+        //     var connectionString =
+        //         "Server=localhost;Database=wtw_task_challenge;Trusted_Connection=True;";
+        //     var connection = new SqlConnection(connectionString);
+        //     var command = connection.CreateCommand();
+        //     command.CommandText = "DELETE FROM tasks WHERE id = 4";
+        //     command.CommandText = "SELECT * FROM tasks WHERE id = 4";
+        //     connection.Open();
+
+        //     // Act
+        //     var reader = command.ExecuteReader();
+
+        //     // Assert
+        //     Assert.True(reader.HasRows);
+        // }
+
         [Fact]
-        public void DeleteRow()
+        public void TestAddTask() 
         {
-            // Arrange
-            var connectionString =
-                "Server=localhost;Database=wtw_task_challenge;Trusted_Connection=True;";
+            ToDoListManager toDoListManager;
+            toDoListManager = new ToDoListManager();
+            Tasks task = new Tasks();
+            task.Id = 1;
+            task.Title = "Test";
+            task.Description = "Description";
+            task.Status = "Not Started";
+            task.Priority = "High";
+            task.Due = new System.DateTime(2021, 11, 05);
+            toDoListManager.AddTask(task);
+
+            var connectionString = "Server=localhost;Database=wtw_task_challenge;Trusted_Connection=True;";
             var connection = new SqlConnection(connectionString);
             var command = connection.CreateCommand();
-            command.CommandText = "DELETE FROM tasks WHERE id = 4";
-            command.CommandText = "SELECT * FROM tasks WHERE id = 4";
+            command.CommandText = "SELECT * FROM tasks WHERE id = 1";
             connection.Open();
-
             // Act
             var reader = command.ExecuteReader();
-
             // Assert
             Assert.True(reader.HasRows);
+        }  
+
+        [Fact]
+        public void TestUpdateTask() 
+        {
+            ToDoListManager toDoListManager;
+            toDoListManager = new ToDoListManager();
+            Tasks task = new Tasks();
+            task.Id = 1;
+            task.Title = "Test Update";
+            task.Description = "Description Update";
+            task.Status = "Not Started";
+            task.Priority = "High";
+            task.Due = new System.DateTime(2021, 11, 05);
+            toDoListManager.UpdateTask(task);
+
+            var connectionString = "Server=localhost;Database=wtw_task_challenge;Trusted_Connection=True;";
+            var connection = new SqlConnection(connectionString);
+            var command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM tasks WHERE id = 1";
+            connection.Open();
+            // Act
+            var reader = command.ExecuteReader();
+            // Assert
+            
+            Assert.True(reader.GetString(3) == "Description Update");
+        }
+
+        // [Fact]
+        // public void TestDeleteTask() 
+        // {
+        //     ToDoListManager toDoListManager;
+        //     toDoListManager = new ToDoListManager();
+        //     Tasks task = new Tasks();
+        //     task.Id = 1;
+        //     toDoListManager.DeleteTask(task);
+        //     var connectionString = "Server=localhost;Database=wtw_task_challenge;Trusted_Connection=True;";
+        //     var connection = new SqlConnection(connectionString);
+        //     var command = connection.CreateCommand();
+        //     command.CommandText = "SELECT * FROM tasks WHERE id = 1";
+        //     connection.Open();
+        //     // Act
+        //     var reader = command.ExecuteReader();
+        //     // Assert
+        //     Assert.False(reader.HasRows);
+        // }
+
+        [Fact]
+        public void TestGetTask() 
+        {
+            ToDoListManager toDoListManager;
+            toDoListManager = new ToDoListManager();
+            Tasks task = new Tasks();
+            task.Id = 1;
+            Tasks retrivedTask = toDoListManager.GetTask(task);
+            // Assert
+            Assert.False(retrivedTask == null);
         }
     }
 }
